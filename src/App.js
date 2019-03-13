7821969f1f81111a64eb625133ebca679a2aaa1d
Raphael Croce <rcroce7@gmail.com>
2019-03-13 00:27:02 -0500

    Set Redux store within app component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View />
+      </Provider>
+    )
+  };
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+}
+
+export default App;

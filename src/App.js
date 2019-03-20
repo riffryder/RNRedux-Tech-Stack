59ebdbfde161bd7697285636a8c4fc96640f86df
Raphael Croce <rcroce7@gmail.com>
2019-03-19 22:46:49 -0500

    Added Header and Library List component to App component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import reducers from './reducers'
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View />
+        <View style={styles.container}>
+          <Header headerText='Tech Stack'/>
+          <LibraryList />
+        </View>
       </Provider>
     )
   };
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
   render() {
+    console.log(this.props);
     return;
   }
 }
 
 const mapStateToProps = (state) => {
-
+  return { libraries: state.libraries };
 }
 
 export default connect(mapStateToProps)(LibraryList);
